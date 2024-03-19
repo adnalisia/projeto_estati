f761b412d501ca54e9f7b610bb2fc8291bbb6613
adnalísia <adna.farias@ufpe.br>
2024-03-19 10:18:44 -0300

    acrescentando outras medidas e histograma

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Ler o arquivo de texto
 df = pd.read_csv('data_1.txt', sep='\t', header=None, names=['dados'])
@@ -7,12 +8,18 @@ df = pd.read_csv('data_1.txt', sep='\t', header=None, names=['dados'])
 print(df)
 # Calculando as medidas de dispersão
 media = round(df['dados'].mean(), 5)
+mediana = round(df['dados'].median(), 5)
+moda = round(df['dados'].mode()[0], 5)
 variancia = round(df['dados'].var(), 5)
 desvio_padrao = round(df['dados'].std(), 5)
 
 #Exibindo os resultados
 print(f'Média: {media}')
+print(f'Mediana: {mediana}')
+print(f'Moda: {moda}')
 print(f'Variância: {variancia}')
 print(f'Desvio Padrão: {desvio_padrao}')
 
-
+#histograma
+df['dados'].plot(kind='hist', bins=20)
+plt.show()
