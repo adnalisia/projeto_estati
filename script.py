b632b1c6c5bedd13afdf0d7f2f56eac00cdc4c39
adnalísia <adna.farias@ufpe.br>
2024-03-19 09:56:03 -0300

    Calculo das medidas de dispersão

--- script.py
+++ script.py
@@ -1,10 +1,18 @@
-from tabulate import tabulate
-
 import pandas as pd
 
-# Leia o arquivo de texto
-df = pd.read_csv('data_1.txt', sep='\t')
+# Ler o arquivo de texto
+df = pd.read_csv('data_1.txt', sep='\t', header=None, names=['dados'])
 
-# Exiba o DataFrame
+# Exibe o DataFrame
 print(df)
+# Calculando as medidas de dispersão
+media = round(df['dados'].mean(), 5)
+variancia = round(df['dados'].var(), 5)
+desvio_padrao = round(df['dados'].std(), 5)
+
+#Exibindo os resultados
+print(f'Média: {media}')
+print(f'Variância: {variancia}')
+print(f'Desvio Padrão: {desvio_padrao}')
+
 
